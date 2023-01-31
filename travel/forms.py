e26a92da139e7ceaba798a69f48db843c7045888
Sebastian <sebastiansloma@gmail.com>
2023-01-31 10:35:21 +0100

    added page - add new venue

--- travel/forms.py
+++ travel/forms.py
@@ -10,3 +10,23 @@ class VenueForm(ModelForm):
         model = Venue
         fields = ('name', 'address', 'zip_code',
                   'phone', 'web', 'email_address')
+        labels = {
+			'name': '',
+			'address': '',
+			'zip_code': '',
+			'phone': '',
+			'web': '',
+			'email_address': '',
+			# 'venue_image': '',			
+		}
+
+        widgets = {
+			'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Venue Name'}),
+			'address': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Address'}),
+			'zip_code': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Zip Code'}),
+			'phone': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}),
+			'web': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Web Address'}),
+			'email_address': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}),
+		}
+        
+
