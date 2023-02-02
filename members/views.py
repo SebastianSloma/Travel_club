2790db9b40081e6fb23cdf778f4c87e5d2d19d07
Sebastian <sebastiansloma@gmail.com>
2023-02-02 18:08:39 +0100

    added others fields to registration

--- members/views.py
+++ members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
 # Create your views here.
@@ -32,7 +33,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -42,7 +43,7 @@ def register_user(request):
             messages.success(request, ('Registration Successful!'))
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
     return render(request, 'authenticate/register_user.html', {
         'form': form
